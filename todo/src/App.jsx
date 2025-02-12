5c4bf29161535fb488273cd4f882acae8d85eea8
Amith Paul <amith.paul@talview.com>
2025-02-12 12:31:50 +0530

    Added Remove feature

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -8,12 +8,20 @@ function App() {
     setTask("");
   }
 
-  console.log(taskList);
+  // console.log(taskList);
   // console.log(task);
 
   function handleChange(e) {
     setTask(e.target.value);
   }
+
+  function deleteTask(taskid) {
+    console.log(taskid);
+    const filteredTasks = taskList.filter((_, id) => id !== taskid);
+    setTasklist(filteredTasks);
+    console.log(taskList);
+  }
+
   return (
     <>
       <div className="items-center justify-center flex">
@@ -36,11 +44,20 @@ function App() {
       <div className="justify-center items-center">
         {taskList.map((item, idx) => (
           <ul key={idx} className="">
-            <li className="py-2 border-1 shadow-lg bg-blue-300">{item}
-              <button className="border-1 px-2 py-1 bg-green-500 text-white rounded-lg  ml-11">Done</button>
-              <button className="border-1 px-2 py-1 bg-red-500 text-white rounded-lg ml-2">Remove</button>
+            <li className="py-2 border-1 shadow-lg bg-blue-300">
+              {item}
+              <button className="border-1 px-2 py-1 bg-green-500 text-white rounded-lg  ml-11">
+                Done
+              </button>
+              <button
+                className="border-1 px-2 py-1 bg-red-500 text-white rounded-lg ml-2"
+                onClick={() => {
+                  deleteTask(idx);
+                }}
+              >
+                Remove
+              </button>
             </li>
-
           </ul>
         ))}
       </div>
