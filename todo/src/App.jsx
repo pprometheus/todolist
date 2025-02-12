b445aa4b310f5d0dace67c5f147f9eedc302d20a
Amith Paul <amith.paul@talview.com>
2025-02-12 12:01:15 +0530

    Created Adding Task feature. Basic UI.

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,12 +1,51 @@
-
+import { useState } from "react";
 
 function App() {
-  
+  const [task, setTask] = useState("");
+  const [taskList, setTasklist] = useState([]);
+  function addTask() {
+    setTasklist((prevTask) => [...prevTask, task]);
+    setTask("");
+  }
+
+  console.log(taskList);
+  // console.log(task);
+
+  function handleChange(e) {
+    setTask(e.target.value);
+  }
   return (
     <>
-      test
+      <div className="items-center justify-center flex">
+        <div>
+          <input
+            className="border-2 p-2"
+            type="text"
+            name=""
+            id=""
+            value={task}
+            placeholder="Enter task"
+            onChange={handleChange}
+          />
+          <button className="border-2 p-2" onClick={addTask}>
+            Add
+          </button>
+        </div>
+      </div>
+
+      <div className="justify-center items-center">
+        {taskList.map((item, idx) => (
+          <ul key={idx} className="">
+            <li className="py-2 border-1 shadow-lg bg-blue-300">{item}
+              <button className="border-1 px-2 py-1 bg-green-500 text-white rounded-lg  ml-11">Done</button>
+              <button className="border-1 px-2 py-1 bg-red-500 text-white rounded-lg ml-2">Remove</button>
+            </li>
+
+          </ul>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
