04df1cf89dd57887669ffaa60542ec810e5685f8
tanishq-khandelwal <tanishqkhandelwal2019@gmail.com>
2025-02-12 17:03:13 +0530

    Added serial number,reset the input box,revert input state

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,23 +1,31 @@
 import { useState } from "react";
 
 function App() {
-  const [task, setTask] = useState({ task: "", status: "false" });//false is boolean
+  const [task, setTask] = useState(""); //false is boolean
   const [taskList, setTasklist] = useState([]);
-  
+
   function addTask() {
     // const completed = "false";
-    if (task != "") { //task.task we are checking task -> object 
-      setTasklist((prevTask) => [...prevTask, task]);
+    if (task != "") {
+      //task.task we are checking task -> object
+      setTasklist((prevTask) => [
+        ...prevTask,
+        {
+          taskName: task,
+          status: false,
+        },
+      ]);
       setTask(""); //setting the object to empty string ?? revert to original state or null
     }
   }
-  console.log("Task",task);
+
+  console.log("Task", taskList);
 
   // console.log(taskList);
   // console.log(task);
 
   function handleChange(e) {
-    setTask({ task: e.target.value, status: false }); 
+    setTask(e.target.value);
   }
 
   function deleteTask(taskid) {
@@ -46,7 +54,7 @@ function App() {
             type="text"
             name=""
             id=""
-            value={task.task}
+            value={task}
             placeholder="Enter task"
             onChange={handleChange}
           />
@@ -63,13 +71,23 @@ function App() {
         {taskList.map((item, idx) => (
           <ul key={idx} className="">
             <li className="py-2  shadow-lg bg-blue-300 px-4 rounded-sm mb-2 flex justify-between">
-              <div className= {item.status ? "line-through" : ""}>
-                {item.task}
+              <div className={item.status ? "line-through flex" : "flex"}>
+                <p>{idx + 1}.</p>
+                <div className="pl-2">{item.taskName}</div>
               </div>
 
               <div className="group relative pl-38 ">
                 <button
-                  className={item.status ? " bg-blue-500" : " bg-green-500"} border-1 px-2 py-1 text-white rounded-lg  ml-11 hover:cursor-pointer invisible group-hover:visible
+                  className={item.status ? " bg-blue-500" : " bg-green-500"}
+                  border-1
+                  px-2
+                  py-1
+                  text-white
+                  rounded-lg
+                  ml-11
+                  hover:cursor-pointer
+                  invisible
+                  group-hover:visible
                   onClick={() => {
                     doneTask(idx);
                   }}
@@ -80,7 +98,7 @@ function App() {
                   className="border-1 px-2 py-1 bg-red-500 text-white rounded-lg ml-2 hover:cursor-pointer invisible group-hover:visible"
                   onClick={() => {
                     deleteTask(idx);
-                  }}  
+                  }}
                 >
                   Remove
                 </button>
